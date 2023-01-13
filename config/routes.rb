d91df67ca98ab629917c7d3cad3d4ca6768cdcf6
825814 <120297399+825814@users.noreply.github.com>
2023-01-13 11:33:49 +0000

    [add]model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
