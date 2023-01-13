0c85b0f1e09a805cf54ae41c54ea8c212f82a25f
825814 <120297399+825814@users.noreply.github.com>
2023-01-13 12:48:07 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +16,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'books/new'
+  post 'books' => 'books#create'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
