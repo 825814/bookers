21cca034de03e7241d3e78131c3097381f7b8176
825814 <120297399+825814@users.noreply.github.com>
2023-01-15 00:59:00 +0000

    [add]削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
   get '/top' => 'homes#top'
+  resources :books
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
