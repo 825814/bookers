e675a2b5253f80008c4a6e3173ad297fb4b86f9d
825814 <120297399+825814@users.noreply.github.com>
2023-01-16 11:55:04 +0000

    [add]updateが失敗したときのバリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
   def create
@@ -24,13 +23,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.new
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = "book was successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
